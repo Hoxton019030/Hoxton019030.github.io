c417883144aab970953aec496d875b926c9e31b0
Hoxton <za546321@yahoo.com.tw>
2023-01-23 20:46:35 +0800

    upload post

--- p/自定義annotation/annotatation/src/main/java/org/example/MyAnnotation.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-import java.lang.annotation.Retention;
-import java.lang.annotation.RetentionPolicy;
-
-@Retention(RetentionPolicy.SOURCE)
-public @interface MyAnnotation {
-}
